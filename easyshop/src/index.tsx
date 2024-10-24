86025c074697b068adbb5c86fb25ef1e48dec69d
xunana <3520653564@qq.com>
2024-10-24 14:24:39 +0800

    feat:配置

--- easyshop/src/index.tsx
+++ easyshop/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // 你可以引入全局样式
+import { API } from './config';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
-
+console.log(API);
 root.render(
   <React.StrictMode>
     <App />
