634da85aa51be379b4078e198b40117f539f5eb4
xunana <3520653564@qq.com>
2024-10-25 18:02:36 +0800

    feat:全局store

--- easyshop/src/components/core/Home.tsx
+++ easyshop/src/components/core/Home.tsx
@@ -1,9 +1,17 @@
-import  Layout  from './Layout';
 import React from 'react';
+import { useSelector } from 'react-redux'; // 从 react-redux 导入 useSelector
+import Layout from './Layout';
 
 const Home = () => {
-    return <Layout>Home</Layout>;
+    const state = useSelector((state) => state); // 获取 Redux 状态，确保访问正确
+
+    return (
+        <Layout>
+            Home {JSON.stringify(state)}
+        </Layout>
+    );
 }
 
 export default Home;
+
 
