2c4f0791b565c5fcafb31f165a2d5ee9dce973bb
xunana <3520653564@qq.com>
2024-10-25 17:41:28 +0800

    feat:add

--- easyshop/src/index.tsx
+++ easyshop/src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // 导入 Provider
+import { configureStore } from '@reduxjs/toolkit'; // 导入 configureStore
 import Routes from './Routes';
+import rootReducer from '../store/reducers/index'; // 导入根 reducer
 
+// 创建 Redux store
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={store}> {/* 将 store 传入 Provider */}
+      <Routes />
+    </Provider>
   </React.StrictMode>
 );
+
 
 
 
